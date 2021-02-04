1c7918431ec775cfaf761367bcee09cc7cde055c
MicmladenTo <micmladento@gmail.com>
2021-02-04 21:36:42 +0100

    Added a test

--- src/tests/DemoTest.java
+++ src/tests/DemoTest.java
@@ -4,18 +4,26 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class DemoTest {
 	
+	WebDriver driver;
 	
-	@Test
+	@BeforeClass
 	public void preKlase() throws IOException {
 		System.setProperty("webdriver.chrome.driver",
 				"driver-lib\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-		
+	}
+	
+	@Test
+	public void testing() throws IOException, InterruptedException {
+	
+	driver.navigate().to("http://cms.demo.katalon.com/");
+	Thread.sleep(2000);
+	driver.close();
 	}
 }
